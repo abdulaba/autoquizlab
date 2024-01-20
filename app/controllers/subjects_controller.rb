e38a54c9ab3f39bd26dfe40a732b78f71464ec1f
eduardo g ed.rf008@gmail.com <ed.rf008@gmail.com>
2024-01-20 15:38:53 -0600

    primer versión de subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,47 +1,45 @@
 class SubjectsController < ApplicationController
-  before_action :set_subject, only: [:show, :edit, :update, :destroy]
-  before_action :set_teacher, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_institution, only: [:new, :create, :edit, :update, :destroy]
 
   def new
-    @subject = Subject.new
+    @subject = @institution.subjects.new
   end
 
   def create
-    @subject = @teacher.subjects.new(subject_params)
+    @subject = @institution.subjects.new(subject_params)
     if @subject.save
-      redirect_to teacher_path(@teacher), notice: 'Subject was successfully created.'
+      redirect_to @institution, notice: 'La materia ha sido creada de manera exitosa.'
     else
       render :new
     end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    @subject = Subject.find(params[:id])
     if @subject.update(subject_params)
-      redirect_to teacher_path(@teacher), notice: 'Subject was successfully updated.'
+      redirect_to @institution, notice: 'La materia ha sido actualizada de manera exitosa.'
     else
       render :edit
     end
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
     @subject.destroy
-    redirect_to teacher_path(@teacher), notice: 'Subject was successfully destroyed.'
+    redirect_to @institution, notice: 'La materia ha sido eliminada de manera exitosa.'
   end
 
   private
 
-  def set_subject
-    @subject = Subject.find(params[:id])
-  end
-
-  def set_teacher
-    @teacher = Teacher.find(params[:teacher_id])
+  def set_institution
+    @institution = Institution.find(params[:institution_id])
   end
 
   def subject_params
-    params.require(:subject).permit(:name, :teacher_id)
+    params.require(:subject).permit(:name, :institution_id)
   end
 end
