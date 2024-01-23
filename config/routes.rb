a4afe28ffb97487c6f2cdd3fee4c6c283fe9914f
Francisco Acuña <facuna490@gmail.com>
2024-01-23 00:04:33 -0600

    teachers controller y views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :teachers
   devise_for :students
   devise_for :teachers
   devise_for :institutions
   root to: "pages#home"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
