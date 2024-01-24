c78c032628f9c64b80386804a40eff52fb582c12
giovanagarcia <giovana.garcu@gmail.com>
2024-01-23 21:01:01 -0600

    Navbar con cerrar sesion

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :independents
+  get 'students/index'
   devise_for :students
   devise_for :teachers
   devise_for :institutions
   root to: "pages#home"
+  resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+
+# devise_for :users, controllers: {
+#   sessions: 'users/sessions',
+#   registrations: 'users/registrations'
+# }
