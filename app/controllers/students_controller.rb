e791214a187d4ba2aeb2613fe3ab36ca4e0327ff
Francisco Acuña <facuna490@gmail.com>
2024-01-27 14:37:52 -0600

    students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,19 @@
 class StudentsController < ApplicationController
+  before_action :authenticate_student!, except: :home
   
   def index
-    @students = Student.all
+    @students = Student.all  
+
+  end
+
+  def show
+    @student = current_student
+    @institution = Institution.find(current_student.institution_id)
   end
 
   def new
     @student = Student.new
-  end
+  end 
 
   def create
     @student = Student.new(student_params)
@@ -16,6 +23,24 @@ class StudentsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @student = current_student
+  end
+
+  def update
+    @student = current_student
+    if @student.update(student_params)
+      redirect_to @student, notice: 'Estudiante ha sido actualizado de manera exitosa.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student.destroy
+    redirect_to students_url, notice: 'Estudiante ha sido eliminado de manera exitosa.'
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 puts "Creando Maestros"
 puts "--------------------------"
 Institution.destroy_all
+Student.destroy_all
+Teacher.destroy_all
 
 Institution.create!(email: "hispano@gmail.com", password: "123456", name: "Hispano Americano de Leon", phone_number: "+1524777326424", address: "Blvd Vasco de Quiroga #235, Leon, Gto, Mexico")
 
@@ -10,43 +12,39 @@ Institution.create!(email: "albatros@gmail.com", password: "123456", name: "Alba
 
 puts "Creando Alumnos"
 puts "---------------------------"
-Student.destroy_all
 
+Student.create!(email: "paquito@gmail.com", password: "123456", institution_id: 12, name: "Francisco Acuña")
 
-Student.create!(email: "paquito@gmail.com", password: "123456", institution_id: 3, name: "Francisco Acuña")
+Student.create!(email: "lalo@gmail.com", password: "123456", institution_id: 13, name: "Eduardo Rios")
 
-Student.create!(email: "lalo@gmail.com", password: "123456", institution_id: 2, name: "Eduardo Rios")
+Student.create!(email: "gio@gmail.com", password: "123456", institution_id: 14, name: "Giovanna Cueto")
 
-Student.create!(email: "gio@gmail.com", password: "123456", institution_id: 1, name: "Giovanna Cueto")
+Student.create!(email: "lulu@gmail.com", password: "123456", institution_id: 12, name: "Maria de Lourdes Rangel")
 
-Student.create!(email: "lulu@gmail.com", password: "123456", institution_id: 1, name: "Maria de Lourdes Rangel")
+Student.create!(email: "sebas@gmail.com", password: "123456", institution_id: 13, name: "Sebastian Gonzalez")
 
-Student.create!(email: "sebas@gmail.com", password: "123456", institution_id: 3, name: "Sebastian Gonzalez")
+Student.create!(email: "emanuel@gmail.com", password: "123456", institution_id: 14, name: "Emanuel Galvan")
 
-Student.create!(email: "emanuel@gmail.com", password: "123456", institution_id: 2, name: "Emanuel Galvan")
+Student.create!(email: "elias@gmail.com", password: "123456", institution_id: 12, name: "Elias Coronado")
 
-Student.create!(email: "elias@gmail.com", password: "123456", institution_id: 1, name: "Elias Coronado")
+Student.create!(email: "fer@gmail.com", password: "123456", institution_id: 13, name: "Fernando Barajas")
 
-Student.create!(email: "fer@gmail.com", password: "123456", institution_id: 3, name: "Fernando Barajas")
+Student.create!(email: "torres@gmail.com", password: "123456", institution_id: 14, name: "Torres")
 
-Student.create!(email: "torres@gmail.com", password: "123456", institution_id: 2, name: "Torres")
-
-Student.create!(email: "leo@gmail.com", password: "123456", institution_id: 2, name: "Leonardo Michell")
+Student.create!(email: "leo@gmail.com", password: "123456", institution_id: 12, name: "Leonardo Michell")
 
 puts "Creando Maestros"
 puts "-------------------------------------"
 
-Teacher.destroy_all
+Teacher.create(email: "laura@gmail.com", password: "123456", name: "Laura Pausini", institution_id: 13)
 
-Teacher.create(email: "laura@gmail.com", password: "123456", name: "Laura Pausini", institution_id: 3)
+Teacher.create(email: "mayela@gmail.com", password: "123456", name: "Maye Rangel", institution_id: 14)
 
-Teacher.create(email: "mayela@gmail.com", password: "123456", name: "Maye Rangel", institution_id: 3)
+Teacher.create(email: "gus@gmail.com", password: "123456", name: "Gustavo Cerati", institution_id: 12)
 
-Teacher.create(email: "gus@gmail.com", password: "123456", name: "Gustavo Cerati", institution_id: 1)
+Teacher.create(email: "flor@gmail.com", password: "123456", name: "Flor Margarita", institution_id: 13)
 
-Teacher.create(email: "flor@gmail.com", password: "123456", name: "Flor Margarita", institution_id: 2)
-
-Teacher.create(email: "cristobal@gmail.com", password: "123456", name: "Cristobal Colon", institution_id: 1)
+Teacher.create(email: "cristobal@gmail.com", password: "123456", name: "Cristobal Colon", institution_id: 14)
 
 puts "Proceso Terminado"
 puts "Seeds creados correctamente :)"
