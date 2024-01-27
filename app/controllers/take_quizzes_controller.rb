fbb4c6b8e2cb441a600e19014c2ed2f9970beb6e
eduardo g ed.rf008@gmail.com <ed.rf008@gmail.com>
2024-01-27 12:42:03 -0600

    cruds agregados y cambio en el controller de quizzes

--- app/controllers/take_quizzes_controller.rb
+++ app/controllers/take_quizzes_controller.rb
@@ -1,5 +1,5 @@
 class TakeQuizzesController < ApplicationController
-  before_action :set_quiz, only: [:new, :create]
+  before_action :set_quiz, only: [:new, :create, :show]
   before_action :authenticate_student!, only: [:new, :create]
 
   def new
@@ -7,14 +7,6 @@ class TakeQuizzesController < ApplicationController
   end
 
   def create
-
-    @take_quiz = TakeQuiz.new(take_quiz_params)
-    @take_quiz.quiz_id = params[:quiz_id]
-    @take_quiz.student_id = current_student.id # assuming you have a current_student method
-
-    if @take_quiz.save
-      redirect_to @take_quiz, notice: 'Haz tomado el examen de manera exitosa.'
-
     @take_quiz = @quiz.take_quizzes.new(take_quiz_params)
     @take_quiz.student = current_student
 
@@ -22,7 +14,7 @@ class TakeQuizzesController < ApplicationController
       score = calculate_score(@take_quiz)
       @take_quiz.update(result: score)
 
-      redirect_to [@quiz, @take_quiz], notice: 'Has completado el quiz.'
+      redirect_to [@quiz, @take_quiz], notice: 'Has completado el examen.'
     else
       render :new
     end
@@ -30,7 +22,6 @@ class TakeQuizzesController < ApplicationController
 
   def show
     @take_quiz = TakeQuiz.find(params[:id])
-    # Assuming @quiz is already set by set_quiz before_action
   end
 
   private
@@ -40,19 +31,14 @@ class TakeQuizzesController < ApplicationController
   end
 
   def take_quiz_params
-
-    params.require(:take_quiz).permit(:result)
-
-    params.require(:take_quiz).permit(:quiz_id, answers_attributes: [:question_id, :selected_option_id])
+    params.require(:take_quiz).permit(answers_attributes: [:question_id, :selected_option_id])
   end
 
   def calculate_score(take_quiz)
     correct_answers_count = 0
 
-    # Assuming each answer submitted has a question_id and selected_option_id
-    take_quiz_params[:answers_attributes].each do |answer_attribute|
-      question = Question.find(answer_attribute[:question_id])
-      correct_answers_count += 1 if question.correct_option_id == answer_attribute[:selected_option_id].to_i
+    take_quiz.answers.each do |answer|
+      correct_answers_count += 1 if answer.question.correct_option_id == answer.selected_option_id
     end
 
     total_questions = @quiz.questions.count
