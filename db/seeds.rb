ab8a18c0b1aa972a3f9293676396cdc2ed33974d
giovanagarcia <giovana.garcu@gmail.com>
2024-02-04 21:08:23 -0600

    Seeds actualizadas al pedo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-puts "Creando Maestros"
+puts "Creando Instituciones"
 puts "--------------------------"
 Institution.destroy_all
 Student.destroy_all
@@ -8,16 +8,15 @@ Subject.destroy_all
 hispano = Institution.create!(email: "hispano@gmail.com", password: "123456", name: "Hispano Americano de Leon", phone_number: "+1524777326424", address: "Blvd Vasco de Quiroga #235, Leon, Gto, Mexico")
 britanico = Institution.create!(email: "britanico@gmail.com", password: "123456", name: "Colegio Britanico", phone_number: "+1524777326425", address: "Av universidad #1235, Campestre, Leon, Guanajuato, Mexico")
 albatros = Institution.create!(email: "albatros@gmail.com", password: "123456", name: "Albatros Bilingual School", phone_number: "+1524777326426", address: "Blvd Paseo de la Reforma #222, Ciudad de Mexico, Mexico")
-reina = Institution.create!(email: "reina@gmail.com", password: "123456", name: "Colegio Reina Isabel", phone_number: "+1524777326426", address: "Calle Sacramento #170, Fracc. Las Palmas, C.P. 22106, Tijuana B.C.")
-cnh = Institution.create!(email: "cnh@gmail.com", password: "123456", name: "Colegio Niños Héroes", phone_number: "+1524777326426", address: "De Las Hojas 16602, Las Huertas 3ra Sección 22115 Tijuana, Península de Baja California, México")
+reina = Institution.create!(email: "reina@gmail.com", password: "123456", name: "Colegio Reina Isabel", phone_number: "+1524777326427", address: "Calle Sacramento #170, Fracc. Las Palmas, C.P. 22106, Tijuana B.C.")
+cnh = Institution.create!(email: "cnh@gmail.com", password: "123456", name: "Colegio Niños Héroes", phone_number: "+1524777326428", address: "De Las Hojas 16602, Las Huertas 3ra Sección 22115 Tijuana, Península de Baja California, México")
 cecc = Institution.create!(email: "cecc@gmail.com", password: "123456", name: "Centro de Estudio en Ciencias de la Comunicacion", phone_number: "+1524777326426", address: "C. Valle 12, Jardines del Pedregal, Álvaro Obregón, 01900 Ciudad de México, CDMX")
-
 
 
 
 puts "Creando Alumnos"
 puts "---------------------------"
-Student.create!(email: "ticzel@gmail.com", password: "123456", institution: cecc, name: "Ruth Zavala")
+Student.create!(email: "ruthz@gmail.com", password: "123456", institution: cecc, name: "Ruth Zavala")
 Student.create!(email: "isabelo@gmail.com", password: "123456", institution: cecc, name: "Isabel Olmos")
 Student.create!(email: "lucian@gmail.com", password: "123456", institution: cecc, name: "Lucia Natalia")
 Student.create!(email: "robertop@gmail.com", password: "123456", institution: cecc, name: "Roberto Piñon")
@@ -66,9 +65,9 @@ puts "-------------------------------------"
 
 lila = Teacher.create(email: "delila@gmail.com", password: "123456", name: "Delila Smith", institution: reina)
 car = Teacher.create(email: "carmen@gmail.com", password: "123456", name: "Carmen Alcaraz", institution: reina)
-lu = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Lucero Cobarruvias", institution: reina)
-ro = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Rocio Mistral", institution: reina)
-kat = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Katia Romero", institution: reina)
+lu = Teacher.create(email: "luceroc@gmail.com", password: "123456", name: "Lucero Cobarruvias", institution: reina)
+ro = Teacher.create(email: "rociomist@gmail.com", password: "123456", name: "Rocio Mistral", institution: reina)
+kat = Teacher.create(email: "katiaromero@gmail.com", password: "123456", name: "Katia Romero", institution: reina)
 
 marc = Teacher.create(email: "mariac@gmail.com", password: "123456", name: "Maria Candelaria", institution: cnh)
 ropo = Teacher.create(email: "robertop@gmail.com", password: "123456", name: "Roberto Povs", institution: cnh)
@@ -76,11 +75,11 @@ lupita = Teacher.create(email: "lupitas@gmail.com", password: "123456", name: "G
 ang = Teacher.create(email: "andresg@gmail.com", password: "123456", name: "Andres Garcia", institution: cnh)
 raulp = Teacher.create(email: "raulp@gmail.com", password: "123456", name: "Raul Politro", institution: cnh)
 
-soc = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Socorro Sanchez", institution: cecc)
-pru = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Prudencia Garcia", institution: cecc)
-edubu = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Eduardo Buenavista", institution: cecc)
-roco = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Rodolfo Cordova", institution: cecc)
-geopa = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Georgina Paz", institution: cecc)
+soc = Teacher.create(email: "socorros@gmail.com", password: "123456", name: "Socorro Sanchez", institution: cecc)
+pru = Teacher.create(email: "prug@gmail.com", password: "123456", name: "Prudencia Garcia", institution: cecc)
+edubu = Teacher.create(email: "eduardob@gmail.com", password: "123456", name: "Eduardo Buenavista", institution: cecc)
+roco = Teacher.create(email: "rodocov@gmail.com", password: "123456", name: "Rodolfo Cordova", institution: cecc)
+geopa = Teacher.create(email: "geopaz@gmail.com", password: "123456", name: "Georgina Paz", institution: cecc)
 
 lau = Teacher.create(email: "laura@gmail.com", password: "123456", name: "Laura Pausini", institution: albatros)
 maye = Teacher.create(email: "mayela@gmail.com", password: "123456", name: "Maye Rangel", institution:hispano)
@@ -91,23 +90,29 @@ cristobal = Teacher.create(email: "cristobal@gmail.com", password: "123456", nam
 puts "Creando Subjects"
 puts "-------------------------------------"
 
-Subject.create(institution: cecc, teacher: soc, name: "Creatividad")
-Subject.create(institution: cecc, teacher: pru, name: "Diseño")
-Subject.create(institution: cecc, teacher: edubu, name: "Contabilidad")
-Subject.create(institution: cecc, teacher: roco, name: "Medios")
-Subject.create(institution: cecc, teacher: geopa, name: "Probabilidad")
+creatividad = Subject.create!(institution: cecc, teacher: soc, name: "Creatividad")
+diseño = Subject.create!(institution: cecc, teacher: pru, name: "Diseño")
+conta = Subject.create!(institution: cecc, teacher: edubu, name: "Contabilidad")
+medios = Subject.create!(institution: cecc, teacher: roco, name: "Medios")
+probabilidad = Subject.create!(institution: cecc, teacher: geopa, name: "Probabilidad")
 
-Subject.create(institution: reina, teacher: lila, name: "Geografia")
-Subject.create(institution: reina, teacher: car, name: "Frances")
-Subject.create(institution: reina, teacher: lu, name: "Comunicacion")
-Subject.create(institution: reina, teacher: ro, name: "Redaccion")
-Subject.create(institution: reina, teacher: kat, name: "Radio")
+geografia = Subject.create!(institution: reina, teacher: lila, name: "Geografia")
+frances = Subject.create!(institution: reina, teacher: car, name: "Frances")
+comunicacion = Subject.create!(institution: reina, teacher: lu, name: "Comunicacion")
+redaccion = Subject.create!(institution: reina, teacher: ro, name: "Redaccion")
+radio = Subject.create!(institution: reina, teacher: kat, name: "Radio")
 
-Subject.create(institution: cnh, teacher: maye, name: "Derecho")
-Subject.create(institution: cnh, teacher: lau, name: "Lectura")
-Subject.create(institution: cnh, teacher: cristobal, name: "Literatura")
-Subject.create(institution: cnh, teacher: gus, name: "Analisis")
-Subject.create(institution: cnh, teacher: flor, name: "Desarrollo")
+derecho = Subject.create!(institution: cnh, teacher: marc, name: "Derecho")
+lectura = Subject.create!(institution: cnh, teacher: ropo, name: "Lectura")
+literatura = Subject.create!(institution: cnh, teacher: lupita, name: "Literatura")
+analisis = Subject.create!(institution: cnh, teacher: ang, name: "Analisis")
+desarrollo = Subject.create!(institution: cnh, teacher: raulp, name: "Desarrollo")
+
+fisica = Subject.create!(institution: hispano, teacher: maye, name: "Fisica")
+historia = Subject.create!(institution: albatros, teacher: lau, name: "Historia")
+cn1 = Subject.create!(institution: hispano, teacher: cristobal, name: "Ciencias Naturales I")
+cn2 = Subject.create!(institution: britanico, teacher: gus, name: "Ciencias Naturales II")
+cn3 = Subject.create!(institution: albatros, teacher: flor, name: "Ciencias Naturales III")
 
 puts "Proceso Terminado"
 puts "Seeds creados correctamente :)"
